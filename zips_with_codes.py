07d6d9e48bd9bd4dd7853c12792807315f761135
Robbie Mitchell <superstrong@users.noreply.github.com>
2021-05-10 22:26:26 -0400

    Fix zip code db import

--- zips_with_codes.py
+++ zips_with_codes.py
@@ -9,9 +9,8 @@ zips_counties_db_dict = dict()
 zips_to_ru_codes_list = []
 
 code_found = set()
-code_fallback_city = set()
 code_fallback_county = set()
-code_fallback_county_zips = set()
+code_fallback_cities_county = set()
 code_missing = set()
 
 
@@ -45,9 +44,14 @@ def cities_with_zips():
                 state = str(row[3]).lower()
                 county = str(row[2]).lower() + "-" + str(row[4]).lower()
                 st_city = st + "-" + city
-                entry = {st_city: [zc]}
+                entry = {st_city: {
+                    'zip': [zc],
+                    'county': [county]
+                    }
+                }
                 if cities_dict.get(st_city) is not None:
-                    if zc not in cities_dict[st_city]: cities_dict[st_city].append(zc)
+                    if zc not in cities_dict[st_city]['zip']: cities_dict[st_city]['zip'].append(zc)
+                    if county not in cities_dict[st_city]['county']: cities_dict[st_city]['county'].append(county)
                 else:
                     cities_dict.update(entry)
     except IOError:
@@ -63,7 +67,7 @@ def cities_with_zips():
 
 
 # Find an RuUr code by zip
-def find_code_for_entry(z):
+def find_ru_code_for_zip(z):
     _code_from_zip = zips_with_details_dict.get(z)
     if _code_from_zip is not None:
         code_found.add(z)
@@ -86,7 +90,7 @@ def generate_zip_details():
                 state = str(row[3]).lower()
                 county = str(row[2]).lower() + "-" + str(row[4]).lower()
                 st_city = st + "-" + city
-                code = find_code_for_entry(zc)
+                code = find_ru_code_for_zip(zc)
                 entry = {zc: {
                     'city': [city],
                     'st': [st],
@@ -113,10 +117,14 @@ def generate_zip_details():
 ## Generated Zip -> County from US Database
 def zips_counties_db():
     try:
-        with open('./source/zip_code_database.csv', newline='') as csvfile:
-            zips = csv.reader(csvfile, delimiter=',', quotechar='|')
+        with open('./source/zip_code_database.txt', newline='') as csvfile:
+            zips = csv.reader(csvfile, delimiter='|', quotechar=None)
+            next(zips)
             for row in zips:
-                zips_counties_db_dict[str(row[0])] = row[17] # zip_code: st-county
+                try:
+                    zips_counties_db_dict[str(row[0])] = row[16] # zip_code: st-county
+                except IndexError:
+                    pass
 
     except IOError:
         print("I/O read error")   
@@ -134,30 +142,47 @@ def counties_with_ru_codes():
                 if counties_dict.get(c) is not None:
                     counties_dict[c]['code'] = min(counties_dict[c]['code'], _selected_code)
                 else:
-                    new_county_entry = {c: {
+                    new_ru_from_county = {c: {
                         'code': _selected_code
                     }}
-                    counties_dict.update(new_county_entry)
+                    counties_dict.update(new_ru_from_county)
 
 
 
 ## Heal Missing Ru-UR Codes with Counties
 def heal_missing_ru_codes():
     for z in zips_dict:
-        c = zips_dict[z].get('code')
-        if not c:
-            f = zips_counties_db_dict.get(z)
-            if f is not None:
-                _county_entry = counties_dict.get(f)
-                if (_county_entry is not None) and (_county_entry['code']):
-                    _code_from_county = _county_entry['code']
-                    code_fallback_county.add(z + " (" + f + ", " + str(_code_from_county) + ")")
-                    zips_dict[z]['code'] = _code_from_county # found a code from county
-                    c = _code_from_county
-                else:
-                    _cities = zips_dict[z].get('city')
-                    code_missing.add(z + " (" + str(_cities) + ")")
-        zips_to_ru_codes_list.append({'zip_code': z, 'rural_urban_code': c})
+        ru_code_from_zip = zips_dict[z].get('code')
+        if ru_code_from_zip:
+            resolved_code = ru_code_from_zip
+        else:
+            st_county = zips_counties_db_dict.get(z)
+            st_county_details = counties_dict.get(st_county)
+            if (st_county_details is not None) and (st_county_details['code']):
+                code_from_county = st_county_details['code']
+                code_fallback_county.add(z + " (" + st_county + ", " + str(code_from_county) + ")")
+                zips_dict[z]['code'] = code_from_county # found a code from county
+                resolved_code = code_from_county
+            else:
+                cities = zips_dict[z].get('st_city')
+                resolved_city_code = False
+                for city in cities: # Loop through every city in the county until a match is found
+                    if resolved_city_code:
+                        break
+                    counties_from_city = cities_dict[city].get('county')
+                    for county_from_city in counties_from_city:
+                        if resolved_city_code:
+                            break
+                        st_county_details = counties_dict.get(county_from_city) # Repeating from above...
+                        if (st_county_details is not None) and (st_county_details['code']):
+                            code_from_county = st_county_details['code']
+                            code_fallback_cities_county.add(z + " (" + county_from_city + ", " + str(code_from_county) + ")")
+                            zips_dict[z]['code'] = code_from_county # found a code from county
+                            resolved_code = code_from_county
+                            resolved_city_code = True
+                if not resolved_city_code:
+                    code_missing.add(z + " (" + str(cities) + ")")
+        zips_to_ru_codes_list.append({'zip_code': z, 'rural_urban_code': resolved_code})
 
 
     # Dump the final dict of zip codes with details to JSON file
